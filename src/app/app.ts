69d3c9c0135b2e7c98cd560bba4a53b39d5dfa6d
shrikantjoshi <joshishrikant812@gmail.com>
2025-08-02 09:35:35 +0530

    added if condition for routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,21 @@
-import { Component, signal, ViewChild } from '@angular/core';
+import { Component, OnInit, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableList } from './shared/shared';
 import { SharedModule } from './shared/shared-module';
 import { MaterialModule } from './coreModules/material.module';
 import { MatSidenav } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SharedModule, MaterialModule],
+  imports: [CommonModule, RouterOutlet, SharedModule, MaterialModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  
+  isLoggedIn = signal(false);
   protected readonly title = signal('my-app');
 
    @ViewChild('sidenav') sidenav!: MatSidenav;
@@ -20,4 +23,15 @@ export class App {
   toggleSidenav() {
     this.sidenav.toggle();
   }
+
+  ngOnInit(): void {
+this.isLoggedIn.set(!!localStorage.getItem('jwt'));
+
+
+  }
+logout() {
+  localStorage.removeItem('jwt');
+  window.location.href = '/login'; // or use router.navigate(['/login']);
+}
+
 }
